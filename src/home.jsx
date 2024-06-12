4898382f515013d8c623ffb435a5926eb9a63911
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2024-06-12 11:26:41 +0100

    initial commit

---
+++ src/home.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './home.css'
+
+function Home() {
+
+  return (
+    <>
+     <p className='font-lora-400 text-3xl'>KLJJKJJKMMM</p>
+    </>
+  )
+}
+
+export default Home
