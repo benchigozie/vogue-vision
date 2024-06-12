aa3e1dae534ab92034e24c4f8f997422bea47b87
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2024-06-13 00:51:21 +0100

    added fonts to vogue projects

---
+++ src/components/nav.jsx
@@ -0,0 +1,9 @@
+function Nav () {
+    return (
+        <>
+        <p className="font-lora font-extralight text-2xl">skjdhdjhMKLK</p>
+        </>
+    )
+}
+
+export default Nav
--- src/home.jsx
+++ src/home.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './home.css'
+import Nav from './components/nav.jsx'
 
 function Home() {
 
   return (
     <>
-     <p className='font-lora-400 text-3xl'>KLJJKJJKMMM</p>
+     <Nav />
+     <p className="font-inter font-extralight text-2xl">skjdhdjhMKLK</p>
     </>
   )
 }
