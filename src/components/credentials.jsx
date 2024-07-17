934251bd1fc30bfd0e97f060886566de5e87a613
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2024-07-18 00:32:37 +0100

    finished the credentials component. it is now fully working

---
+++ src/components/credentials.jsx
@@ -0,0 +1,22 @@
+function Credentials () {
+    return (
+        <>
+        <div className="flex justify-around gap-3 px-4">
+            <div>
+                <p className="text-2xl font-semibold text-center">5+</p>
+                <p className=" text-sm alg:text-base text-center">Years of Experience</p>
+            </div>
+            <div>
+                <p className="text-2xl font-semibold text-center">98%</p>
+                <p className="text-sm alg:text-base text-center">Customer Satisfaction Rate</p>
+            </div>
+            <div>
+                <p className="text-2xl font-semibold text-center">150+</p>
+                <p className="text-sm alg:text-base text-center">Satisfied Customers</p>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Credentials;
